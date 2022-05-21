c62ef6a7d46cdd18179aaceca4d23b837ff313fc
Tazaev <tonymurtazaev@gmail.com>
2022-05-20 23:22:44 -0400

    added java questions

---
+++ assets/qu-quest.js
@@ -0,0 +1,57 @@
+//var quiz questions here
+
+var = Quizquestion = [
+    {
+        question: "Which of the below is the correct way to write a JavaScript array?", 
+        answers: [
+            { text: "var vegetable = ['onion', 'apple', 'spinach']", correct: true },
+            { text: "var vegetable = (0:'tomato', 1:'basil', 2:'spinach')", correct: false },
+            { text: "var vegetable = (garlic, lettuce, spinach)", correct: false },
+            { text: "None of the above", correct: false }
+        ]
+    },
+
+    {
+        question: "Which of the choices below call a function for each element in the array?", 
+        answers: [
+            { text: "filter()", correct: false },
+            { text: "concat()", correct: false },
+            { text: "split()", correct: false },
+            { text: "forEach()", correct: true }
+        ]
+
+    },
+
+    {
+        question: "How do you round the number 102.456, to the nearest integer?",
+        answers: [
+            { text: "Math.random(102.456)", correct: false },
+            { text: "Math.rnd(102.456)", correct: false },
+            { text: "round(102.456)", correct: false },
+            { text: "Math.round(102.456)", correct: true }
+        ]
+
+    },
+
+    {
+        question: "which choice below is an IF statement for executing some code if 'i' is NOT equal to 5?", 
+        answers: [
+            { text: "if (i <> 5)", correct: false },
+            { text: "if (i != 5)", correct: true },
+            { text: "if i =! 5", correct: false },
+            { text: "if (i !=== 5)", correct: false }
+        ]
+    },
+
+    {
+        question: "How do you write 'Good Day' in an alert box?", 
+        answers: [
+            { text: "alertBox('Good Day')", correct: false },
+            { text: "msg('Good Day')", correct: false },
+            { text: "alert('Good Day')" , correct: true },
+            { text: "prompt('Good Day')", correct: false }
+            
+        ]
+    },
+
+];
